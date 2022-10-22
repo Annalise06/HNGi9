02edb84a450f4a882a5ff2cd1e7cd4dc027e5ab2
Anna Miene <tarerie00annie@gmail.com>
2022-10-22 21:36:28 +0100

    profile section completed

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
+import "./App.css";
+import Avatar from "@mui/material/Avatar";
+import Avg from "./girl.jpg";
+
 function App() {
   return (
     <div className="App">
-      <h1>Starting My First Task</h1>
+      <div className="profile">
+        <Avatar
+          className="profile_img"
+          alt="Remy Sharp"
+          src={Avg}
+          sx={{ width: 72, height: 72 }}
+        />
+      </div>
+      <p className="profile_name">Annette Black</p>
     </div>
   );
 }
